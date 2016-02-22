0dbc122cc4e41c6b424b88bd843117e961631c37
Geoff Bones <geoff.bones@siemens.com>
2016-02-22 11:00:04 +0100

    Initial commit

---
+++ BowlingGameTests/BowlingGameTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace BowlingGameTests
+{
+    public class BowlingGameTests
+    {
+        [Test]
+        public void GivenTwenty0sBowled_WhenGameScoreCalculated_ShouldBe0()
+        {
+            
+        }
+    }
+}
